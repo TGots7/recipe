5bf53e25da8e2970cb2312573aba352f14bffa75
TGots7 <tylergotto17@gmail.com>
2017-10-02 17:07:26 -0400

    partials and cleaning

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :instructions
+  validates :nickname, presence: true
   validates :email, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
